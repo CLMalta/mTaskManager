20494232a97e09ef079c8ca10c7bcc7b09f2abfd
JaroslawKurowski <jaroslaw.kurowski@gmail.com>
2024-04-06 17:19:10 +0200

    ADD Jwt

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,11 @@
+using System.Text;
+using Application.Services;
 using Domain.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistence.Context;
+using Persistence.Repository;
 using WebApi.Auth;
 
 
@@ -16,8 +22,56 @@ namespace WebApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TaskManager WebApi", Version = "v1" });
+
+                // Konfiguracja Swaggera do u¿ywania tokenów Bearer
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Proszê wprowadziæ token JWT z prefiksem 'Bearer' w polu",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"]
+                    };
+
+                });
+            builder.Services.AddAuthorization();
+
             builder.Services.AddSingleton<IDapperContext, DapperContext>();
+            builder.Services.AddSingleton<IUserRepository, UserRepository>();
+            builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddSingleton<JwtTokenService>();
+
             builder.Services.AddSingleton<LoggedUser>(provider => new LoggedUser(1, "admin", "admin@example.com", UserRole.Administrator));
             var app = builder.Build();
 
@@ -30,8 +84,8 @@ namespace WebApi
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
